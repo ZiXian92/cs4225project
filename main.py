ccfd1cafaf79e4c7d03d14abf34057ade89f6ea5
zixian92 <zixian1992@hotmail.com>
2017-04-05 12:48:40 +0800

    Starts changing to use libsvm for RBF and adds some documentation for end result of transformations

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from svmutil import svm_train, svm_predict
 from pyspark import SparkContext, SparkConf
 from pyspark.sql import SparkSession
 from pyspark.mllib.classification import SVMWithSGD
@@ -17,6 +18,26 @@ def sampleK(itemList, k):
                 sampleList[token] = item
     return sampleList
 
+# Options to be passed to SVM for training
+svm_options = '-s 0 -t 2 -c 10'
+
+# rddEntry format: (key, [[[training_labels], [training_data]], [[expected_labels], [test_data]]])
+# Output format: (numFailedPredictions, expectedFailedPredictions, numFalseAlarms, numGoodRecords)
+def getPredictionStats(rddEntry):
+    model = svm_train(rddEntry[1][0][0], rddEntry[1][0][1], svm_options)
+    labels, acc, values = svm_predict(rddEntry[1][1][0], rddEntry[1][1][1], model)
+    numFailedPredictions, expectedFailedPredictions, numFalseAlarms, numGoodRecords = 0, 0, 0, 0
+    for idx prediction in enumerate(labels):
+        if rddEntry[1][1][0][idx]==1:
+            expectedFailedPredictions+=1
+            if labels[idx]==rddEntry[1][1][0][idx]:
+                numFailedPredictions+=1
+        else:
+            numGoodRecords+=1
+            if labels[idx]!=rddEntry[1][1][0][idx]:
+                numFalseAlarms+=1
+    return (numFailedPredictions, expectedFailedPredictions, numFalseAlarms, numGoodRecords)
+
 # Prepare desired columns
 desiredsmartnos = [1, 3, 5, 7, 9, 187, 189, 194, 195, 197]
 desiredcolumns = ['date', 'serial_number', 'model', 'failure']
@@ -33,7 +54,7 @@ if __name__ == "__main__":
     # Then parition by individual hard disk and sort by date so we can
     # model partition as time series and compute rate of change of attributes.
     drivedatadf = sparksql.read.csv('/user/zixian/project/input/*.csv', inferSchema = True, header = True)
-    drivedatadf = drivedatadf.select(desiredcolumns)
+    drivedatadf = drivedatadf.select(desiredcolumns).fillna(0)
     # drivedatadf = drivedatadf.repartition('serial_number', 'model')
     drivedatadf.cache()
 
@@ -59,57 +80,70 @@ if __name__ == "__main__":
     # Compute rate of change of raw values of the 2 smart values(how?)
     # Leaving this out for now. Prioritizing whole flow of SVM
 
+    # Data extractions and transformations begin here!
     # Get records for good training drives
     traingoodrdd = drivedatadf.rdd.filter(lambda r: (r.serial_number, r.model) in traingoodset.value)
     # Keyed by drive identifier
     traingoodrdd = traingoodrdd.map(lambda r: ((r.serial_number, r.model), r)).groupByKey().mapValues(list)
-    # Pick 4 sample records per good drive for training
+    # Pick 4 sample records per good drive for training. Output format: Row
     traingoodrdd = traingoodrdd.flatMap(lambda r: sampleK(r[1], 4))
-    # Convert records to LabeledPoint
-    traingoodrdd = traingoodrdd.map(lambda r: LabeledPoint(0, map(lambda col: r[col], desiredcolumns[4:])), True)
+    # Convert to (model, [0, features])
+    traingoodrdd = traingoodrdd.map(lambda r: (r.model, [0]+map(lambda col: r[col], desiredcolumns[4:])))
 
     # Get records for failed training drives
     trainfailedrdd = drivedatadf.rdd.filter(lambda r: (r.serial_number, r.model) in trainfailedset.value)
-    # Keyed by drive identifier and group records by drive identifier
+    # Outputformat: ((sn, model), Row)
     trainfailedrdd = trainfailedrdd.map(lambda r: ((r.serial_number, r.model), r)).groupByKey().mapValues(list)
-    # Sort all records by record's date and extract last 10 days of operations
+    # Sort all records by record's date and extract last 10 days of operations. Output format: Row
     trainfailedrdd = trainfailedrdd.flatMap(lambda r: sorted(r[1], None, lambda p: p.date, True)[:10])
-    # Convert records to LabeledPoint
-    trainfailedrdd = trainfailedrdd.map(lambda r: LabeledPoint(1, map(lambda col: r[col], desiredcolumns[4:])), True)
-    trainingrdd = traingoodrdd.union(trainfailedrdd)
+    # Output format: (model, [1, features])
+    trainfailedrdd = trainfailedrdd.map(lambda r: (r.model, [1]+map(lambda col: r[col], desiredcolumns[4:])))
+
+    # Combine the training data sets
+    # Output format: (model, [[0 or 1, features]])
+    trainingrdd = traingoodrdd.union(trainfailedrdd).groupByKey().mapValues(list)
 
     # Get records for good test drives
-    testgoodrdd = drivedatadf.rdd.filter(lambda r: (r.serial_number, r.model) in testgoodset.value)
+    # testgoodrdd = drivedatadf.rdd.filter(lambda r: (r.serial_number, r.model) in testgoodset.value)
     # Convert records to data for SVM
-    testgoodrdd = testgoodrdd.map(lambda r: map(lambda col: r[col], desiredcolumns[4:]), True)
-    testgoodrdd.cache()
+    # testgoodrdd = testgoodrdd.map(lambda r: map(lambda col: r[col], desiredcolumns[4:]), True)
+    # testgoodrdd.cache()
+    # testgoodrdd.repartition(1).saveAsTextFile('/user/zixian/project/test-good-data')
 
     # Get records for failed test drives
-    testfailedrdd = drivedatadf.rdd.filter(lambda r: (r.serial_number, r.model) in testfailedset.value)
+    # testfailedrdd = drivedatadf.rdd.filter(lambda r: (r.serial_number, r.model) in testfailedset.value)
     # Keyed by drive identifier and groups records by drive identifier
-    testfailedrdd = testfailedrdd.map(lambda r: ((r.serial_number, r.model), r)).groupByKey().mapValues(list)
+    # testfailedrdd = testfailedrdd.map(lambda r: ((r.serial_number, r.model), r)).groupByKey().mapValues(list)
     # Sort all records by record's date and extract last 10 days of operations
-    testfailedrdd = testfailedrdd.flatMap(lambda r: sorted(r[1], None, lambda p: p.date, True)[:10])
+    # testfailedrdd = testfailedrdd.flatMap(lambda r: sorted(r[1], None, lambda p: p.date, True)[:10])
     # Convert records to data for SVM
-    testfailedrdd = testfailedrdd.map(lambda r: map(lambda col: r[col], desiredcolumns[4:]), True)
-    testfailedrdd.cache()
+    # testfailedrdd = testfailedrdd.map(lambda r: map(lambda col: r[col], desiredcolumns[4:]), True)
+    # testfailedrdd.cache()
     # testrdd = testgoodrdd.union(testfailedrdd)
+    # testfailedrdd.repartition(1).saveAsTextFile('/user/zixian/project/test-failed-data')
 
     # Done preprocessing all records. Proceed to free up memory.
     drivedatadf.unpersist()
 
     # Train the SVM model
-    svm = SVMWithSGD.train(trainingrdd)
+    # svm = SVMWithSGD.train(trainingrdd)
+    # svm.setThreshold(0.5)
 
     # Run predictions
-    predictedFailures = svm.predict(testfailedrdd).reduce(lambda a, b: a+b)
-    actualFailures = testfailedrdd.count()
-    predictionRate = predictedFailures*100.0/actualFailures
-    testfailedrdd.unpersist()
+    # mocktestrdd = trainfailedrdd.map(lambda lp: lp.features)
+    # trainfailedrdd.unpersist()
+    # mocktestrdd.cache()
+    # mocktestrdd.repartition(1).saveAsTextFile('/user/zixian/project/mock-test-failed-data')
+    # svm.predict(mocktestrdd).repartition(1).saveAsTextFile('/user/zixian/project/failed-drive-prediction')
+    # mocktestrdd.unpersist()
+    # predictedFailures = svm.predict(testfailedrdd).reduce(lambda a, b: a+b)
+    # actualFailures = testfailedrdd.count()
+    # predictionRate = predictedFailures*100.0/actualFailures
+    # testfailedrdd.unpersist()
 
-    predictedFailures = svm.predict(testgoodrdd).filter(lambda x: x==0).count()
-    falseAlarmRate = predictedFailures*100.0/testgoodrdd.count()
-    testgoodrdd.unpersist()
+    # predictedFailures = svm.predict(testgoodrdd).filter(lambda x: x==0).count()
+    # falseAlarmRate = predictedFailures*100.0/testgoodrdd.count()
+    # testgoodrdd.unpersist()
 
-    print 'Prediction Rate: ', str(predictionRate)+'%'
-    print 'False Alarm Rate: ', str(falseAlarmRate)+'%'
+    # print 'Prediction Rate: ', str(predictionRate)+'%'
+    # print 'False Alarm Rate: ', str(falseAlarmRate)+'%'
