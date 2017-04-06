1b38fb069b7e0631426e734c7f3ce059f690db77
Tjong Anthony <tjonganthony@gmail.com>
2017-04-06 18:48:08 +0800

    change key to manufacturer instead of model

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ from pyspark.sql import SparkSession
 from datetime import date
 import random
 
+LOGGER = None
+
 # convert model(3rd column of csv file) into model enum
 def convertModel(model):
-    if model[:2] == 'Hi' or model[:4] == 'HGST':
+    if model[:2] == 'Hi':
         # hitachi
         return 0
     elif model[:2] == 'ST':
@@ -15,9 +17,12 @@ def convertModel(model):
     elif model[:3] == 'WDC':
         # western digital
         return 2
+     or model[:4] == 'HGST':
+        # hgst
+        return 3
     elif model[:7] == 'TOSHIBA':
         # toshiba
-        return 3
+        return 4
     else:
         raise Exception()
 
@@ -60,9 +65,18 @@ def sampleK(itemList, k):
                 sampleList[token] = item
     return sampleList
 
+counter = 0
+def test(a, b):
+    print(counter)
+    global counter
+    counter += 1
+    print(a)
+    print(b)
+    return (a[0]+b[0], a[1]+b[1], a[2]+b[2], a[3]+b[3])
+
 # rddEntry format: (key, [[[training_labels], [training_data]], [[expected_labels], [test_data]]])
 # Output format: (numFailedPredictions, expectedFailedPredictions, numFalseAlarms, numGoodRecords)
-def getPredictionStats(rddEntry):
+def getPredictionStats(rddEntry, logger = LOGGER):
     if len(rddEntry[1])<2:
         return (0, 0, 0, 0)
 
@@ -82,11 +96,16 @@ def getPredictionStats(rddEntry):
                 numFalseAlarms+=1
     return (numFailedPredictions, expectedFailedPredictions, numFalseAlarms, numGoodRecords)
 
+def temp(a, b):
+    print('a:' + str(a))
+    print('b:' + str(b))
+    return (a[0]+b[0], a[1]+b[1], a[2]+b[2], a[3]+b[3])
+
 # Prepare desired columns
 desiredsmartnos = [1, 3, 5, 7, 9, 194, 197]
 desiredcolumns = ['date', 'serial_number', 'model', 'failure']
 for sno in desiredsmartnos:
-    desiredcolumns.append('smart_'+str(sno)+'_normalized')
+    # desiredcolumns.append('smart_'+str(sno)+'_normalized')
     desiredcolumns.append('smart_'+str(sno)+'_raw')
 
 if __name__ == "__main__":
@@ -95,32 +114,42 @@ if __name__ == "__main__":
     sparkcontext.addFile('hdfs://ec2-34-204-54-226.compute-1.amazonaws.com:9000/libsvm-322', True)
     sparksql = SparkSession.builder.master('local').appName('hddpredict').getOrCreate()
 
+    log4jLogger = sparkcontext._jvm.org.apache.log4j
+    global LOGGER
+    LOGGER = log4jLogger.LogManager.getLogger(__name__)
+
+
     # Load the entire data and project wanted columns
     # Then parition by individual hard disk and sort by date so we can
     # model partition as time series and compute rate of change of attributes.
     # drivedatadf = sparksql.read.csv('/user/zixian/project/input/*.csv', inferSchema = True, header = True)
-    drivedatadf = sparksql.read.csv('hdfs://ec2-34-204-54-226.compute-1.amazonaws.com:9000/data/*.csv', inferSchema = True, header = True)
+    drivedatadf = sparksql.read.csv('hdfs://ec2-34-204-54-226.compute-1.amazonaws.com:9000/data/2016-11-1*.csv', inferSchema = True, header = True)
     drivedatadf = drivedatadf.select(desiredcolumns).fillna(0)
     drivedatadf.cache()
+    print(1)
 
     # Get list of distinct drives
     distinctdrivesdf = drivedatadf.groupBy('serial_number', 'model').agg({'failure': 'max'}).withColumnRenamed('max(failure)', 'failure').select('serial_number', 'model', 'failure')
     distinctdrivesdf.cache()
+    print(2)
 
     # Generate list of good and bad drives
     faileddrivedf = distinctdrivesdf.filter('failure = 1').distinct().select('serial_number', 'model')
     gooddrivesdf = distinctdrivesdf.filter('failure = 0').distinct().select('serial_number', 'model')
     distinctdrivesdf.unpersist()
+    print(3)
 
     # Split failed drives into testing and training
     trainfaileddf, testfaileddf = faileddrivedf.randomSplit([0.7, 0.3])
     trainfailedset = sparkcontext.broadcast(set(trainfaileddf.rdd.map(lambda r: (r.serial_number, r.model)).collect()))
     testfailedset = sparkcontext.broadcast(set(testfaileddf.rdd.map(lambda r: (r.serial_number, r.model)).collect()))
+    print(4)
 
     # Split good drives into testing and training
     traingooddf, testgooddf = gooddrivesdf.randomSplit([0.7, 0.3])
     traingoodset = sparkcontext.broadcast(set(traingooddf.rdd.map(lambda r: (r.serial_number, r.model)).collect()))
     testgoodset = sparkcontext.broadcast(set(testgooddf.rdd.map(lambda r: (r.serial_number, r.model)).collect()))
+    print(5)
 
     # Data extractions and transformations begin here!
     # Get records for good training drives. Output format: Row.
@@ -130,44 +159,50 @@ if __name__ == "__main__":
     # Pick 4 sample records per good drive for training. Output format: Row
     traingoodrdd = traingoodrdd.flatMap(lambda r: sampleK(r[1], 4))
     # Convert to (model, [0, features]). Change the key as needed.
-    traingoodrdd = traingoodrdd.map(lambda r: (r.model, [0]+map(lambda col: r[col], desiredcolumns[4:])))
+    traingoodrdd = traingoodrdd.map(lambda r: (convertModel(r.model), [0]+map(lambda col: r[col], desiredcolumns[4:])))
+    print(6)
 
     # Get records for failed training drives. Output format: Row.
     trainfailedrdd = drivedatadf.rdd.filter(lambda r: (r.serial_number, r.model) in trainfailedset.value)
     # Outputformat: ((sn, model), Row)
     trainfailedrdd = trainfailedrdd.map(lambda r: ((r.serial_number, r.model), r)).groupByKey().mapValues(list)
     # Sort all records by record's date and extract last 10 days of operations. Output format: Row
-    trainfailedrdd = trainfailedrdd.flatMap(lambda r: sorted(r[1], None, lambda p: p.date, True)[:10])
+    trainfailedrdd = trainfailedrdd.flatMap(lambda r: sorted(r[1], None, lambda p: p.date, True)[:4])
     # Output format: (key, [1, features]). Change the key as needed.
-    trainfailedrdd = trainfailedrdd.map(lambda r: (r.model, [1]+map(lambda col: r[col], desiredcolumns[4:])))
+    trainfailedrdd = trainfailedrdd.map(lambda r: (convertModel(r.model), [1]+map(lambda col: r[col], desiredcolumns[4:])))
+    print(7)
 
     # Combine the training data sets. This part should not need any changes.
     # Output format: (key, [[0 or 1, features]])
     trainingrdd = traingoodrdd.union(trainfailedrdd).groupByKey().mapValues(list)
     # Output format: (key, [[training_labels], [[features]]])
     trainingrdd = trainingrdd.map(lambda r: (r[0], [map(lambda x: x[0], r[1]), map(lambda x: x[1:], r[1])]))
+    print(8)
 
     # Get records for good test drives. Output format: Row.
     testgoodrdd = drivedatadf.rdd.filter(lambda r: (r.serial_number, r.model) in testgoodset.value)
     # Any further processing if needed
     # Output format: (key, [0, features]). Change the key as you need.
-    testgoodrdd = testgoodrdd.map(lambda r: (r.model, [0]+map(lambda col: r[col], desiredcolumns[4:])))
+    testgoodrdd = testgoodrdd.map(lambda r: (convertModel(r.model), [0]+map(lambda col: r[col], desiredcolumns[4:])))
+    print(9)
 
     # Get records for failed test drives.
     testfailedrdd = drivedatadf.rdd.filter(lambda r: (r.serial_number, r.model) in testfailedset.value)
     # Extract last 10 days of records. Output format: ((sn, model), Row). Add futher processing as needed.
     testfailedrdd = testfailedrdd.map(lambda r: ((r.serial_number, r.model), r)).groupByKey().mapValues(list)
     # Sort all records by record's date and extract last 10 days of operations. Output format: Row
-    testfailedrdd = testfailedrdd.flatMap(lambda r: sorted(r[1], None, lambda p: p.date, True)[:10])
+    testfailedrdd = testfailedrdd.flatMap(lambda r: sorted(r[1], None, lambda p: p.date, True)[:4])
     # Output format: (key, [1, features]). Change the key as you need.
-    testfailedrdd = testfailedrdd.map(lambda r: (r.model, [1]+map(lambda col: r[col], desiredcolumns[4:])))
+    testfailedrdd = testfailedrdd.map(lambda r: (r.convertModel(r.model), [1]+map(lambda col: r[col], desiredcolumns[4:])))
     # End of modifiable transformations
+    print(10)
 
     # Combine test data sets. Should not need any changes.
     # Output format: (key, [0 or 1, features])
     testrdd = testgoodrdd.union(testfailedrdd).groupByKey().mapValues(list)
     # Output format: (key, [[expected_labels], [[features]]])
     testrdd = testrdd.map(lambda r: (r[0], [map(lambda x: x[0], r[1]), map(lambda x: x[1:], r[1])]))
+    print(11)
 
     # Combine training and test data set. union and groupByKeys seem to preserve ordering of values on collect().
     # This part shouldn't need any change.
@@ -176,9 +211,12 @@ if __name__ == "__main__":
 
     # Done preprocessing all records. Proceed to free up memory.
     drivedatadf.unpersist()
+    print(12)
 
     # Run SVM per key
-    predictionStats = modellingrdd.map(getPredictionStats).reduce(lambda a, b: (a[0]+b[0], a[1]+b[1], a[2]+b[2], a[3]+b[3]))
+    print('haha'*100)
+    print(modellingrdd.take(10))
+    predictionStats = modellingrdd.map(getPredictionStats).reduce(test)
     print predictionStats
     # print 'Predicted RDD'
     # Outputs [(0, 0, 0, 21)], which is of correct format
